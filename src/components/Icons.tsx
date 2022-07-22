bcee81a71e9f2027bb733e231ed73534ddda0b83
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-07-23 01:50:31 +0200

    icons and shit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,13 @@
+import {FaDiscord, FaGithub, FaTwitter} from 'react-icons/fa';
+
+const Icons = () => {
+    return (
+        <div className="flex flex-row gap gap-3">
+            <FaDiscord />
+            <FaGithub />
+            <FaTwitter />
+        </div>
+    );
+    }
+
+export default Icons;
