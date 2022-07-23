2d172d35144f03cae82637085359406f28d34136
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-07-23 22:48:56 +0200

    gimme that inch tho

--- src/hooks/useInch.ts
+++ src/hooks/useInch.ts
@@ -1,3 +1,33 @@
+import { LimitOrderProtocolFacade, Web3ProviderConnector } from "@1inch/limit-order-protocol";
+// import { Web3Provider } from "@ethersproject/providers";
+import Web3 from 'web3';
 export const Inch = () => {
     console.log("calling inch");
 }
+
+const baseContract = '0x5fa31604fc5dcebfcac2481f9fa59d174126e5e6'
+// const walletAddress = '0x5fa31604fc5dcebfcac2481f9fa59d174126e5e6'
+
+const createLimitOrderFacade = (limitContractAddresss:string, provider:any) => { 
+    const connector = new Web3ProviderConnector(provider);
+    //  address of orderbook contract on the network
+    new LimitOrderProtocolFacade(
+        limitContractAddresss,
+        connector
+    );
+    
+
+
+
+
+}
+    
+
+export const listCoveredCall = async () => {
+    console.log("list covered call");
+
+}
+
+
+
+
