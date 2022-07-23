4d2fa8569986e2fde0f72926ed62ecba837d82ac
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-07-23 10:38:24 +0200

    adding some more hooks and scaffolding for some componnetns

---
+++ src/hooks/useCov.ts
@@ -0,0 +1,9 @@
+// This file is used for interacting with the cov contract
+// Functionality of this includes loading cov data,
+//  writing cov contracts with collateral, and
+
+
+export const writeCov = async () => {
+    // Get the current account
+    console.log("writing cov");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,23 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'black': '#000000',
+        'black': '#111111',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    },
+    plugins: [require('daisyui')],
+  }
 }
