7dd633441127dcbadeb6ddda140412549ddf97ae
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-07-29 16:18:24 -0700

    added some of the utils

---
+++ src/utils/chains.ts
@@ -0,0 +1,29 @@
+
+export const chains = [
+    {
+        id: '0x1',
+        name: 'Ethereum Mainnet',
+        rpc: "",
+        symbol: "ETH",
+    },
+    {
+        id: '0x3',
+        name: 'Ropsten',
+        rpc: "https://ropsten.infura.io/v3/",
+        symbol: "ROP",
+    },
+    {
+        name: 'Celo (Mainnet)',
+        id: '0x4220',
+        rpc: 'https://baklava-forno.celo-testnet.org',
+        symbol: 'CELO',
+    },
+    {
+        name: 'Celo (Testnet - Alfa)',
+        id: '0x44787',
+        rpc: 'https://alfajores-forno.celo-testnet.org',
+        symbol: 'CELO',
+    },
+    
+
+]
